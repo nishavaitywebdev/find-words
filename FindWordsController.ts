cabaf385818e1ac418d8a7c01993298a6bf6a509
Nisha Vaity <nishavaity7@gmail.com>
2024-03-03 16:19:56 -0800

    Added details to run tests and start the application in the README. Also, added comments in the code to explain the functionality

--- FindWordsController.ts
+++ FindWordsController.ts
@@ -1,14 +1,36 @@
-const checkIfWordCanForm = (w: string, characterMap: Map<string, number>) => {
+const checkIfWordCanBeFormed = (w: string, characterMap: Map<string, number>) => {
     const map = {};
     for (let char of w) {
+        /* If any letter of word is not present in the map,
+        then word cannot be formed, we return false.
+        */
         if (!characterMap.has(char)) return false;
+
+        /**
+         * Else we need to check if the count of the letters
+         * also match the count of character in the map. So we 
+         * track the letter count
+         */
         map[char] = (map[char] || 0) + 1;
+
+        /*
+        If at any point the letter count exceeds the count in map,
+        we return false;
+        */
         if (characterMap.get(char) < map[char]) return false;
     }
+
+    /**
+     * If all the letters are present and the count also matches,
+     * then word can be formed, finally, we return true. 
+     */
     return true;
 }
 
 export const findWords = (inputString: string, dictionary: string[]): string[] => {
+    /* collect all letters of the input string and their count 
+    and store in the map 
+    */
     let characterMap = new Map();
     for (let char of inputString) {
         const count = characterMap.get(char) || 0;
@@ -16,9 +38,15 @@ export const findWords = (inputString: string, dictionary: string[]): string[] =
     }
     const result = [];
 
+    /**
+     * We iterate through all the words of the dictionary and check if 
+     * the word can be formed from the input string. If word 
+     * can be formed, then we store it in the results array and 
+     * return the result at the end of all iterations. 
+     */
     for (let word of dictionary) {
-        const wordFormed = checkIfWordCanForm(word, characterMap);
-        if (wordFormed) result.push(word);
+        const isWordFormed = checkIfWordCanBeFormed(word, characterMap);
+        if (isWordFormed) result.push(word);
     }
     return result;
 };
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # find-words
+
+## Objective 
+This application helps find list of words from the dictionary that can be formed by rearranging some or all of the letters in the input string. For example, 
+
+```
+findWords("ate", ["ate", "eat", "tea", "dog", "do", "god", "goo", "go", "good"]);
+// Expected output: ["ate", "eat", "tea"]
+
+```
+
+## Start application
+
+Before you get started, make sure you have node installed and configured correctly. 
+
+To build, open your terminal and navigate to the `javascript` directory in this project and run `npm install`.
+
+## Testing
+
+To run unit tests run `npm test`.
+
